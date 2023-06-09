a617ed3192ec8a3c31a9e09676f5ed62f29add4e
arunsai <mangarapuarunsai225@gmail.com>
2023-06-09 17:30:52 +0000

    E bank

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {Switch, Redirect, Route} from 'react-router-dom'
+
+import Login from './Login'
+import NotFound from './NotFound'
+import ProtectedRoute from './ProtectedRoute'
+import Home from './Home'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/ebank/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
